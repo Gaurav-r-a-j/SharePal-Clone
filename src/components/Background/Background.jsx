37999fbb64ebe6818b401b54bd2f42a866dc3cf1
sstarx <gk5271@srmist.edu.in>
2022-08-21 14:51:50 +0530

    completed

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./Background.css"
 
 const Background = () => {
   return (
     <div className="backgroundContainer">
         <div className="strip">
-            <p>Delivery Date <button>Delivery Date</button></p>
+            <p>Delivery Date <button className='stripBtn'>Delivery Date</button></p>
+            <p>Return Date <button className='stripBtn'>Return Date</button></p>
+            <p><button className='stripBtn yBtn'>View Prices</button></p>
         </div>
     </div>
   )
